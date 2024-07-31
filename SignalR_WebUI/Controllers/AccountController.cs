9121e0d62e90cdab04ca28dfed8b93ceca4a6216
Bberkayyy <bberkayoguz@gmail.com>
2024-07-31 15:42:37 +0300

    Ufak düzenlemeler

--- SignalR_WebUI/Controllers/AccountController.cs
+++ SignalR_WebUI/Controllers/AccountController.cs
@@ -7,15 +7,40 @@ using SignalR_WebUI.Dtos.IdentityDtos;
 
 namespace SignalR_WebUI.Controllers;
 [AllowAnonymous]
+[Route("User")]
 public class AccountController : Controller
 {
     private readonly SignInManager<AppUser> _signInManager;
+    private readonly UserManager<AppUser> _userManager;
 
-    public AccountController(SignInManager<AppUser> signInManager)
+    public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
     {
         _signInManager = signInManager;
+        _userManager = userManager;
+    }
+    [Route("Regsiter")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+    [HttpPost]
+    [Route("Regsiter")]
+    public async Task<IActionResult> Register(RegisterDto registerDto)
+    {
+        AppUser appUser = new()
+        {
+            Name = registerDto.Name,
+            Surname = registerDto.Surname,
+            Email = registerDto.Mail,
+            UserName = registerDto.Username,
+        };
+        IdentityResult result = await _userManager.CreateAsync(appUser, registerDto.Password);
+        if (result.Succeeded)
+            return RedirectToAction("Login", "User");
+        return View();
     }
     [HttpGet]
+    [Route("Login")]
     public IActionResult Login([FromQuery(Name = "ReturnUrl")] string returnUrl = "/")
     {
         return View(new LoginDto
@@ -24,6 +49,7 @@ public class AccountController : Controller
         });
     }
     [HttpPost]
+    [Route("Login")]
     public async Task<IActionResult> Login(LoginDto loginDto)
     {
         var result = await _signInManager.PasswordSignInAsync(loginDto.Username, loginDto.Password, false, false);
@@ -31,9 +57,10 @@ public class AccountController : Controller
             return Redirect(loginDto.ReturnUrl ?? "/");
         return View();
     }
+    [Route("Logout")]
     public async Task<IActionResult> Logout()
     {
         await _signInManager.SignOutAsync();
-        return RedirectToAction("Login", "Account");
+        return RedirectToAction("Login", "User");
     }
 }
