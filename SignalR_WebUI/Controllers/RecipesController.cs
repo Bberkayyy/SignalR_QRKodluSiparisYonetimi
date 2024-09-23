0d8853e86882da650483b242d09e0a6f2208c942
Bberkayyy <bberkayoguz@gmail.com>
2024-09-23 17:16:46 +0300

    product recipes were taken from tasty rapidApi.

---
+++ SignalR_WebUI/Controllers/RecipesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR_WebUI.Dtos.RecipeDtos;
+
+namespace SignalR_WebUI.Controllers;
+
+[AllowAnonymous]
+public class RecipesController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var client = new HttpClient();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=60&tags=under_30_minutes"),
+            Headers =    {{ "x-rapidapi-key", "ff9b850119msh8af7061f39bd390p1c72c5jsneb3f1285ac62" },
+                          { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+            },
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            ResultRecipeDto? values = JsonConvert.DeserializeObject<ResultRecipeDto>(body);
+            return View(values);
+        }
+    }
+}
---
+++ SignalR_WebUI/Dtos/RecipeDtos/ResultRecipeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR_WebUI.Dtos.RecipeDtos;
+
+public class ResultRecipeDto
+{
+    public int Count { get; set; }
+    public List<RecipesDto>? Results { get; set; }
+
+    public class RecipesDto
+    {
+        public string Name { get; set; }
+        public string Original_Video_Url { get; set; }
+        public int Total_Time_Minutes { get; set; }
+        public string Thumbnail_Url { get; set; }
+    }
+}
